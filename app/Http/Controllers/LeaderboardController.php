592b1ecfcf1fe375834660da1ec52b68a942dbf2
Bart Roos <bart@studiokaa.co>
2022-09-05 20:18:23 +0200

    Fix leaderboard

--- app/Http/Controllers/LeaderboardController.php
+++ app/Http/Controllers/LeaderboardController.php
@@ -14,13 +14,33 @@ class LeaderboardController extends Controller
     }
 
     public function show(Game $game)
-    {
-        // $teams = $game->teams->sortBy('score', SORT_NUMERIC);
-        $teams = $game->teams->sortBy(function ($team, $key) {
-            return $team->getScoreAttribute();
-        }, SORT_NUMERIC);
-        $teams->values()->all();
-        return $teams;//->get();
-        //return view('score_game')->with(compact('game'))->with(compact('teams'));
+    {   
+        $teams = $game->teams->toArray();
+        usort($teams, function ($a, $b){
+            return intval($b['score']) <=> intval($a['score']);
+        });
+
+        $from = [$game->teams->min('score'), $game->teams->max('score')];
+        array_walk($teams, [$this, 'mapColor'], $from);
+
+        return view('score_game')->with(compact('game'))->with(compact('teams'));
+    }
+
+    public function mapColor(&$team, $key, $from) {
+        $toLow = 95;
+        $toHigh = 25;
+        $fromLow = $from[0];
+        $fromHigh = $from[1];
+
+        $fromRange = $fromHigh - $fromLow;
+        $toRange = $toHigh - $toLow;
+        $scaleFactor = $toRange / $fromRange;
+    
+        // Re-zero the value within the from range
+        $tmpValue = $team['score'] - $fromLow;
+        // Rescale the value to the to range
+        $tmpValue *= $scaleFactor;
+        // Re-zero back to the to range
+        $team['color'] = $tmpValue + $toLow;
     }
 }

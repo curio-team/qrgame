c0bf9bf19ce199cc0e837b7a247dd88c3c558de0
Bart Roos <bart@studiokaa.co>
2022-09-05 22:13:29 +0200

    Update assignment2.blade.php

--- resources/views/assignment2.blade.php
+++ resources/views/assignment2.blade.php
@@ -8,6 +8,6 @@
 <h1>Opdracht</h1>
 <p><em style="text-decoration: underline;">Let op:</em> als je deze pagina ververst of afsluit verdwijnt de opdracht. Jij bent de enige die deze opdracht kan zien, dus <em>jij</em> moet zorgen dat deze opdracht wordt uitgevoerd.</p>
 
-<p style="background: lightblue; padding: 20px; margin-top: 20px; font-weight: 600;">{{ $question->text }}</p>
+<p style="background: lightblue; padding: 20px; margin-top: 20px;"><strong>{{ $question->text }}</strong><br><br>Leg het resultaat vast op foto of film en laat dat zien aan de jury om hiervoor punten te krijgen!</p>
 
 @endsection
